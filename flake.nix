53beb96ce6a098daf7902460258b4aef68e7c4ca
Manu [tennox] <148720-TeNNoX@users.noreply.gitlab.com>
2023-02-28 18:44:16 +0100

    👷 [CI] pipeline that builds all templates as a test
    The build will fail because the devshell.overlay is deprecated,
    but I want to write a broken test first

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
           pkgs = import nixpkgs {
             inherit system;
 
-            overlays = [ devshell.overlay ];
+            overlays = [ devshell.overlays.default ];
           };
         in
         pkgs.devshell.mkShell {
